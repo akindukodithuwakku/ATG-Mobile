8aa83c64b180161edb1e4d6c04c2b8dc317d1536
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2024-12-27 12:41:59 +0530

    Updated navigation from App.js

--- App.js
+++ App.js
@@ -4,6 +4,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 // Import screens
 import WelcomeScreen from "./screens/WelcomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+// import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+// import ProfileScreen from './screens/ProfileScreen';
+// import ReadinessQuestionnaireScreen from './screens/ReadinessQuestionnaireScreen';
+// import AppointmentSchedulingScreen from './screens/AppointmentSchedulingScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +18,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        {/* <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="ReadinessQuestionnaire" component={ReadinessQuestionnaireScreen} />
+        <Stack.Screen name="AppointmentScheduling" component={AppointmentSchedulingScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
