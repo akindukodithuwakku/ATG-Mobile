17c1b6d55f3b3547e3d51c3d0a562a8c5683bee5
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2025-02-09 22:13:42 +0530

    Add new screens to navigation and update stack navigator configuration.

--- App.js
+++ App.js
@@ -7,23 +7,31 @@ import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 // import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
-// import ProfileScreen from './screens/ProfileScreen';
+import ProfileScreen from './screens/ProfileScreen';
 // import ReadinessQuestionnaireScreen from './screens/ReadinessQuestionnaireScreen';
 // import AppointmentSchedulingScreen from './screens/AppointmentSchedulingScreen';
+import CarePlanMgt from './screens/CarePlanMgt';
+import MedicationMgt from './screens/MedicationMgt';
+import ClientDashboard from './screens/ClientDashboard';
+import CNDashboard from './screens/CNDashboard';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
-        {/* <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        {/* <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} /> */}
         <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="ReadinessQuestionnaire" component={ReadinessQuestionnaireScreen} />
+        {/* <Stack.Screen name="ReadinessQuestionnaire" component={ReadinessQuestionnaireScreen} />
         <Stack.Screen name="AppointmentScheduling" component={AppointmentSchedulingScreen} /> */}
+        <Stack.Screen name="CarePlan" component={CarePlanMgt} />
+        <Stack.Screen name="Medication" component={MedicationMgt} />
+        <Stack.Screen name="ClientDashboard" component={ClientDashboard} />
+        <Stack.Screen name="CNDashboard" component={CNDashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
