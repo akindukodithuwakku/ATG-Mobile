0b3767ee19dea592a6bc999f4599ca2ab3e49f0f
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2025-03-18 12:20:12 +0530

    Add .env file and AWS Amplify config for authentication setup.

---
+++ aws-config.js
@@ -0,0 +1,9 @@
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    region: process.env.REACT_APP_REGION,
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+  }
+});
