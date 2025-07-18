49979a367be680fbed1182d2dc0318848de04125
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-07-18 09:57:28 +0530

    add pages

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="MedicationC"
+        initialRouteName="DocumentsCN"
         screenOptions={{ headerShown: false }}eas login
 
       >
